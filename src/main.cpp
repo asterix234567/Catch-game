ce047bcb81a93a9c71982cccea91a1108021a9f7
asterix234567 <max.m.bauer@gmail.com>
2025-03-03 20:43:06 +0100

    Initial from template

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <raylib.h>
+
+
+int main() 
+{
+    InitWindow(800, 800, "First raylib project");
+    SetTargetFPS(60);
+
+    while(!WindowShouldClose())
+    {
+
+    }
+
+    CloseWindow();
+    return 0;
+}
